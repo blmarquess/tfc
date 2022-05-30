b9afcccc70a3879a4ccea33d999b072c12ac6a9b
blmarquess <blmarques.dev@gmail.com>
2022-05-30 18:43:20 -0300

    start

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -2,29 +2,31 @@ import * as express from 'express';
 
 class App {
   public app: express.Express;
-  // ...
 
   constructor() {
-    // ...
+    this.app = express();
     this.config();
-    // ...
+    this.routes();
   }
 
   private config():void {
     const accessControl: express.RequestHandler = (_req, res, next) => {
       res.header('Access-Control-Allow-Origin', '*');
-      res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS,PUT,PATCH');
+      res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS,PUT');
       res.header('Access-Control-Allow-Headers', '*');
       next();
     };
 
     this.app.use(accessControl);
-    // ...
+    this.app.use(express.json());
   }
 
-  // ...
+  private routes(): void {
+
+  }
+
   public start(PORT: string | number):void {
-    // ...
+    this.app.listen(PORT, () => console.log(`executando na porta ${PORT}`));
   }
 }
 
