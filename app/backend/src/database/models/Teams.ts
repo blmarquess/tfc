e66c78f7758c34385a0c48dc4856edf0fa7b2cc3
Bruno Marques <blmarques.dev@gmail.com>
2022-06-07 21:05:13 -0300

    feat(migration-model): adding Model And migration for Team

---
+++ app/backend/src/database/models/Teams.ts
@@ -0,0 +1,27 @@
+import { Model, DataTypes } from 'sequelize';
+import db from '.';
+
+class Teams extends Model { }
+
+Teams.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    underscored: true,
+    sequelize: db,
+    modelName: 'Teams',
+    timestamps: false,
+  },
+);
+
+export default Teams;
