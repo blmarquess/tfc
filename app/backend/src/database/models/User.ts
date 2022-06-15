789a86fdd659031ce66cfb5b8f1bff5468588b81
Bruno Marques <blmarques.dev@gmail.com>
2022-06-15 19:35:40 -0300

    feat(models): created model for users

---
+++ app/backend/src/database/models/User.ts
@@ -0,0 +1,35 @@
+import { DataTypes, Model } from 'sequelize';
+import db from '.';
+
+class User extends Model {}
+
+User.init({
+  id: {
+    type: DataTypes.STRING,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  role: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'User',
+  timestamps: false,
+});
+
+export default User;
