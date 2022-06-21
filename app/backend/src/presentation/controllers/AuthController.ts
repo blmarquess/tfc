eaff4332695ebc1af2d9675355386f8098431267
Bruno Marques <blmarques.dev@gmail.com>
2022-06-20 21:17:54 -0300

    feat(auth): created auth controller

--- app/backend/src/presentation/controllers/AuthController.ts
+++ app/backend/src/presentation/controllers/AuthController.ts
@@ -1,3 +1,16 @@
-// export default class AuthController {
-//   constructor(public authService) {}
-// }
+import { Request, Response } from 'express';
+import { verifyAuthToken } from '../../useCases/authUseCase/AuthServiceSection';
+
+export default class AuthController {
+  constructor(private HttpRequest: Request, private response: Response) {
+    this.HttpRequest = HttpRequest;
+    this.response = response;
+  }
+
+  public async verify() {
+    const { authorization } = this.HttpRequest.headers;
+
+    const decoded = verifyAuthToken(authorization as string);
+    return this.response.status(201).json(decoded);
+  }
+}
