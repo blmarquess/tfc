8defbd3a5a92418967d8c4f2c1cf83f457f0b624
Bruno Marques <blmarques.dev@gmail.com>
2022-06-20 21:19:59 -0300

    feat(login): created router for validate type credencial logged

--- app/backend/src/presentation/routers/loginRouter.ts
+++ app/backend/src/presentation/routers/loginRouter.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import RouterAdapter from '../adapters/ExpressRouterAdapter';
 import LoginFactory from '../factories/LoginFactory';
+import UserValidationFactory from '../factories/UserValidationFactory';
 
 const LoginRouter = Router();
 
@@ -9,5 +10,7 @@ LoginRouter.get('/', (_req, res, _next) => res.status(200).json({
 }));
 
 LoginRouter.post('/', RouterAdapter(LoginFactory));
+
+LoginRouter.get('/validate', RouterAdapter(UserValidationFactory));
 
 export default LoginRouter;
