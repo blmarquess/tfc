26d8b2b9373dad3bbd1d9b04a41ed8a8e97b0ffa
Bruno Marques <blmarques.dev@gmail.com>
2022-06-24 09:20:10 -0300

    feat(repository): created new method on createMatch Reposiroty

--- app/backend/src/useCases/repositories/ICreateMatchesRepository.ts
+++ app/backend/src/useCases/repositories/ICreateMatchesRepository.ts
@@ -2,5 +2,6 @@ import { MatchEntity } from '../../entities/MatchEntity';
 import Match from '../../database/models/Match';
 
 export interface ICreateMatchesRepository {
+  verifyIfExistTeams(num:number, num2:number): Promise<boolean>;
   execute(newMatch: MatchEntity): Promise<Match>;
 }
--- app/backend/src/useCases/repositories/implementations/CreateMatchesRepository.ts
+++ app/backend/src/useCases/repositories/implementations/CreateMatchesRepository.ts
@@ -1,9 +1,20 @@
 import { MatchEntity } from '../../../entities/MatchEntity';
+import { TeamEntity } from '../../../entities/TeamEntity';
+
 import Match from '../../../database/models/Match';
+import Team from '../../../database/models/Teams';
+
 import { ICreateMatchesRepository } from '../ICreateMatchesRepository';
 
 export default class CreateMatchesRepository implements ICreateMatchesRepository {
   private repository = Match;
+  private teamRepository = Team;
+
+  async verifyIfExistTeams(num: number, num2: number) {
+    const match = await this.teamRepository
+      .findAll({ where: { id: [num, num2] } }) as unknown as TeamEntity[];
+    return match.length !== 2;
+  }
 
   async execute(newMatch: MatchEntity) {
     const createdMatch = await this.repository.create(newMatch);
