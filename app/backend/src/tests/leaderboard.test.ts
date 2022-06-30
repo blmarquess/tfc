85996186c8e36512eab26b7ca3533c0824ac0f9c
Bruno Marques <blmarques.dev@gmail.com>
2022-06-30 02:23:17 -0300

    fix(totalpoints): ajust calc to points of team on match

---
+++ app/backend/src/tests/leaderboard.test.ts
@@ -0,0 +1,18 @@
+// @ts-ignore
+import chaiHttp = require('chai-http');
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+import Match from '../database/models/Match';
+import { Response } from 'superagent';
+
+const { expect } = chai;
+
+import { app } from '../app';
+
+chai.use(chaiHttp);
+
+describe('Teams Success cases all Leaderboard', () => {
+  it('should be', async () => {
+    
+  });
+});
